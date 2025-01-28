50e6ba5171f432c51a3e07578f162f87dd685a26
EvgenSharapov <dooplert@yandex.ru>
2025-01-28 20:54:12 +0300

    init

--- src/main/java/org/example/config/AppConfig.java
+++ src/main/java/org/example/config/AppConfig.java
@@ -1,78 +1,10 @@
 package org.example.config;
 
-import com.zaxxer.hikari.HikariDataSource;
-import jakarta.persistence.EntityManagerFactory;
-import org.hibernate.cfg.Environment;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.support.JdbcTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import javax.sql.DataSource;
-import java.util.Properties;
-
 
 @Configuration
 @ComponentScan("org.example")
-@EnableTransactionManagement
 public class AppConfig {
-
-
-    @Value("${db.driver}")
-    private String dbDriver;
-
-    @Value("${db.url}")
-    private String dbUrl;
-
-    @Value("${db.username}")
-    private String dbUsername;
-
-    @Value("${db.password}")
-    private String dbPassword;
-
-    @Bean
-    public DataSource dataSource(){
-        HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setDriverClassName(dbDriver);
-        dataSource.setJdbcUrl(dbUrl);
-        dataSource.setUsername(dbUsername);
-        dataSource.setPassword(dbPassword);
-        dataSource.setMaximumPoolSize(10);
-        return dataSource;
-
-    }
-    @Bean
-    public Properties properties() {
-        Properties properties = new Properties();
-        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
-        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-        properties.put(Environment.HBM2DDL_AUTO,"validate");
-
-        return properties;
-    }
-
-
-    @Bean
-    public LocalSessionFactoryBean sessionFactoryBean(DataSource dataSource,Properties properties){
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-        sessionFactory.setPackagesToScan("org.example.domain");
-        sessionFactory.setHibernateProperties(properties);
-        return sessionFactory;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory factory){
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(factory);
-        return transactionManager;
-    }
-
 
 }
--- src/main/java/org/example/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.domain.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}

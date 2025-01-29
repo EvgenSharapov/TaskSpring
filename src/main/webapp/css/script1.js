c1840fa5e2d556b9fbce85dd980d143fbfcf1b24
EvgenSharapov <dooplert@yandex.ru>
2025-01-29 14:26:01 +0300

    изменение структуры сайта, добавление стилей

--- src/main/webapp/css/script1.js
+++ src/main/webapp/css/script1.js
@@ -16,42 +16,39 @@ function showList(page_number) {
     }
 
     $.get(url, function (response) {
-        // remove all existing rows
-        $("tr:has(td)").remove();
+        $("tr:has(td)").remove(); // Очистка таблицы перед добавлением новых данных
 
-        //arr accounts data
         $.each(response, function (i, item) {
             $('<tr>').html("<td>"
                 + item.id + "</td><td>"
                 + item.description + "</td><td>"
                 + item.status + "</td><td>"
                 + "<button class='edit-btn' id='button_edit_" + item.id + "' onclick='editAcc(" + item.id + ")'>"
-                // + "<img src='/img/edit.png' class='img_edit'>"
                 + "</button>"
                 + "<button class='delete-btn' id='button_delete_" + item.id + "' onclick='deleteAcc(" + item.id + ")'>"
-                // + "<img src='/img/delete.png' class='img_delete'>"
                 + "</button>" + "</td>")
                 .appendTo('#taskTable');
         });
     });
 
+    // Получение общего количества задач для пагинации
     let totalCount = getTotalCount();
     let pagesCount = Math.ceil(totalCount / countPerPage);
 
-    // remove all existing paging buttons
-    $("button.pgn-bnt-styled").remove();
+    // Очистка старых кнопок пагинации
+    $("#paging_buttons").empty();
 
-    //add paging buttons
+    // Добавление кнопок пагинации
     for (let i = 0; i < pagesCount; i++) {
-        let button_tag = "<button>" + (i + 1) + ("</button>");
+        let button_tag = "<button>" + (i + 1) + "</button>";
         let btn = $(button_tag)
             .attr('id', "paging_button_" + i)
             .attr('onclick', "showList(" + i + ")")
             .addClass('pgn-bnt-styled');
-        $('#paging_buttons').append(btn);
+        $("#paging_buttons").append(btn);
     }
 
-    // mak current page
+    // Выделение текущей страницы
     if (page_number !== null) {
         let identifier = "#paging_button_" + page_number;
         $(identifier).css("color", "red").css("font-weight", "bold");
@@ -72,6 +69,7 @@ function getTotalCount() {
     return parseInt(value);
 }
 
+
 function deleteAcc(id) {
     let url = "/rest/tasks/" + id;
     $.ajax({
@@ -87,34 +85,28 @@ function editAcc(id) {
     let identifier_edit = "#button_edit_" + id;
     let identifier_delete = "#button_delete_" + id;
 
-    // Удаляем кнопку "Delete"
     $(identifier_delete).remove();
 
-    // Сохраняем ссылку на родительский элемент (строку таблицы) до замены кнопки
     let current_tr_element = $(identifier_edit).closest('tr');
 
-    // Заменяем кнопку "Edit" на кнопку "Save"
     let save_button_tag = "<button class='save-btn' onclick='saveAcc(" + id + ")'></button>";
     $(identifier_edit).replaceWith(save_button_tag);
 
-    // Получаем дочерние элементы строки таблицы
     let children = current_tr_element.children();
 
-    // Заменяем содержимое ячейки с именем на текстовое поле
     let td_name = children[1];
     td_name.innerHTML = "<input id='input_name_" + id + "' type='text' value='" + td_name.innerHTML.trim() + "'>";
 
-    // Заменяем содержимое ячейки с расой на выпадающий список
-    let td_race = children[2];
-    let race_current_value = td_race.innerHTML.trim();
-    td_race.innerHTML = getDropdownRaceHtml(id);
-    $("#select_race_" + id).val(race_current_value).change();
+    let td_status = children[2];
+    let status_current_value = td_status.innerHTML.trim();
+    td_status.innerHTML = getStatusHtml(id);
+    $("#select_status_" + id).val(status_current_value).change();
 
 }
 
 function saveAcc(id) {
     let value_name = $("#input_name_" + id).val();
-    let value_race = $("#select_race_" + id).val();
+    let value_race = $("#select_status_" + id).val();
 
     let url = "/rest/tasks/" + id;
     $.ajax({
@@ -130,40 +122,7 @@ function saveAcc(id) {
     });
 }
 
-function createAcc() {
-    let value_name = $("#input_name_new").val();
-    let value_title = $("#input_title_new").val();
-    let value_race = $("#input_race_new").val();
-    let value_profession = $("#input_profession_new").val();
-    let value_level = $("#input_level_new").val();
-    let value_birthday = $("#input_birthday_new").val();
-    let datetime = new Date(value_birthday).getTime();
-    let value_banned = $("#input_banned_new").val();
 
-    let current_page = window.location.href;
-    console.log(current_page);
-
-    let url = "/rest/players";
-    $.ajax({
-        url: url,
-        type: 'POST',
-        dataType: 'json',
-        contentType: 'application/json;charset=UTF-8',
-        async: false,
-        data: JSON.stringify({"name": value_name, "title": value_title, "race": value_race, "profession": value_profession, "level": value_level, "birthday": datetime, "banned": value_banned}),
-        success: function () {
-            $("#input_name_new").val("");
-            $("#input_title_new").val("");
-            $("#input_race_new").val();
-            $("#input_profession_new").val();
-            $("#input_level_new").val("");
-            $("#input_birthday_new").val("");
-            $("#input_banned_new").val(false);
-            showList(getCurrentPage());
-        }
-    });
-    return false;
-}
 
 function getCurrentPage() {
     let current_page = 1;
@@ -176,35 +135,36 @@ function getCurrentPage() {
     return parseInt(current_page) - 1;
 }
 
-function getDropdownRaceHtml(id) {
-    let race_id = "select_race_" + id;
-    return "<label for='race'></label>"
-        + "<select id=" + race_id + " name='race'>"
+function getStatusHtml(id) {
+    let status_id = "select_status_" + id;
+    return "<label for='status'></label>"
+        + "<select id=" + status_id + " name='status' class='status-select'>"
         + "<option value='DONE'>DONE</option>"
         + "<option value='PAUSED'>PAUSED</option>"
         + "<option value='IN_PROGRESS'>IN_PROGRESS</option>"
+        + "</select>";
 
 }
 
-    function addTask() {
-        let description = $("#description").val();
-        let status = $("#status").val();
+function addTask() {
+    let description = $("#description").val();
+    let status = $("#status").val();
 
-        $.ajax({
-            url: "/rest/tasks",
-            type: 'POST',
-            dataType: 'json',
-            contentType: 'application/json;charset=UTF-8',
-            async: false,
-            data: JSON.stringify({ "description": description, "status": status }),
-            success: function () {
-                $("#description").val("");
-                $("#status").val("");
-                showList(currentPage); // Обновляем таблицу после добавления задачи
-            }
-        });
-        return false;
-
+    $.ajax({
+        url: "/rest/tasks",
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/json;charset=UTF-8',
+        async: false,
+        data: JSON.stringify({ "description": description, "status": status }),
+        success: function () {
+            $("#description").val("");
+            $("#status").val("");
+            showList(getCurrentPage()); // Обновление списка и пагинации
+        }
+    });
+    return false;
 }
+
 
 
